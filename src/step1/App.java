e9c7f937ecbc5fe5cdb99af11c4ac5ca00f88464
ChoiT815 <choi081517@gmail.com>
2026-03-20 19:52:06 +0900

    step1 계산기 구현

---
+++ src/step1/App.java
@@ -0,0 +1,61 @@
+package step1;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        while (true) {
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = scanner.nextInt();
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = scanner.nextInt();
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String op = scanner.next();
+            char opChar = op.charAt(0);
+
+            int result = 0;
+
+            switch (opChar) {
+                case '+':
+                    result = num1 + num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("양의 정수를 입력해주세요!");
+                    } else {
+                        result = num1 / num2;
+                        System.out.println("결과: " + result);
+                    }
+                    break;
+                default:
+                    System.out.println("잘못된 연산자 입력입니다. 다시 입력해주세요!");
+                    break;
+
+            }
+            System.out.print("더 계산하시겠습니까? (종료:exit) : ");
+            String answer = scanner.next();
+            if (answer.equals("exit")) {
+                break;
+            }
+        }
+
+    }
+
+}
